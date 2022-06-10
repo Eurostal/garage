ab625e03eb0d979e06df51bf23705d11611869bc
Kamil <kamil@webo.agency>
2022-06-10 16:41:08 +0200

    window frame shadows

--- src/configurator/window.js
+++ src/configurator/window.js
@@ -4,28 +4,33 @@ import { MeshStandardMaterial, MeshPhysicalMaterial, ExtrudeGeometry, Mesh, Grou
 export default class Window extends WallElement {
   constructor(width, height, name) {
     super(width, height, new MeshStandardMaterial({ color: 0xeeeeee, side: DoubleSide }), name);
+    this.object = this.createWindow();
+  }
+
+  createWindow() {
     const windowShape = new Shape();
     windowShape.moveTo(0, 0);
-    windowShape.lineTo(0, height);
-    windowShape.lineTo(width, height);
-    windowShape.lineTo(width, 0);
+    windowShape.lineTo(0, this.height);
+    windowShape.lineTo(this.width, this.height);
+    windowShape.lineTo(this.width, 0);
 
     const windowInnerPath = new Path();
     windowInnerPath.moveTo(0.05, 0.05);
-    windowInnerPath.lineTo(0.05, height - 0.05);
-    windowInnerPath.lineTo(width - 0.05, height - 0.05);
-    windowInnerPath.lineTo(width - 0.05, 0.05);
+    windowInnerPath.lineTo(0.05, this.height - 0.05);
+    windowInnerPath.lineTo(this.width - 0.05, this.height - 0.05);
+    windowInnerPath.lineTo(this.width - 0.05, 0.05);
 
     windowShape.holes = [windowInnerPath];
 
     const frameGeometry = new ExtrudeGeometry(windowShape, { depth: 0.06, bevelEnabled: false });
     const frameMesh = new Mesh(frameGeometry, this.material);
     frameMesh.receiveShadow = true;
-    frameMesh.translateX(-width / 2);
+    frameMesh.castShadow = true;
+    frameMesh.translateX(-this.width / 2);
     frameMesh.translateZ(-0.03);
 
     const glassMesh = new Mesh(
-      new BoxGeometry(width - 0.1, height - 0.1, 0.02),
+      new BoxGeometry(this.width - 0.1, this.height - 0.1, 0.02),
       new MeshPhysicalMaterial({
         color: 0x4a6363,
         metalness: 0.3,
@@ -37,13 +42,13 @@ export default class Window extends WallElement {
         side: DoubleSide,
       })
     );
-    glassMesh.translateY(height / 2);
+    glassMesh.translateY(this.height / 2);
 
     const window = new Group();
     window.add(frameMesh);
     window.add(glassMesh);
     window.name = this.name;
 
-    this.object = window;
+    return window;
   }
 }
