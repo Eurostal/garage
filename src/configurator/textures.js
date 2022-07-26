2ec060e2c7c135eee8e1041d72633af2fe6ccd38
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-07-26 13:28:33 +0200

    Vie config update

--- src/configurator/textures.js
+++ src/configurator/textures.js
@@ -1,91 +1,103 @@
-import { TextureLoader, RepeatWrapping } from "three";
+import { TextureLoader, RepeatWrapping } from 'three'
+import imgUrlmetalTexture from '/src/assets/metal/metal.png'
+import imgUrlmetalTextureMap from '/src/assets/metal/metal-normal.png'
+import imgUrlmetalTextureRoughness from '/src/assets/metal/metal-roughness.png'
+import imgUrlmetalTextureHeight from '/src/assets/metal/metal-height.png'
+import imgUrlmetalTextureWide from '/src/assets/metal/metal-wide.png'
+import imgUrlmetalTextureWideMap from '/src/assets/metal/metal-wide-normal.png'
+import imgUrlwoodTexture from '/src/assets/wood/wood.png'
+import imgUrlwoodTextureWide from '/src/assets/wood/wood-wide.png'
+import imgUrlwoodTextureMap from '/src/assets/wood/wood-normal.png'
+import imgUrlwoodTextureWideMap from '/src/assets/wood/wood-wide-normal.png'
+import imgUrlgrassTexture from '/src/assets/grass.jpg'
+import imgUrlconcreteTexture from '/src/assets/concrete.jpg'
 
-const loader = new TextureLoader();
+const loader = new TextureLoader()
 
-export const metalTexture = loadTexture("./src/assets/metal/metal.png");
-metalTexture.rotation = Math.PI / 2;
-metalTexture.wrapS = RepeatWrapping;
-metalTexture.wrapT = RepeatWrapping;
-metalTexture.matrixAutoUpdate = true;
+export const metalTexture = loadTexture(imgUrlmetalTexture)
+metalTexture.rotation = Math.PI / 2
+metalTexture.wrapS = RepeatWrapping
+metalTexture.wrapT = RepeatWrapping
+metalTexture.matrixAutoUpdate = true
 
-export const metalTextureMap = loadTexture("./src/assets/metal/metal-normal.png");
-metalTextureMap.rotation = Math.PI / 2;
-metalTextureMap.wrapS = RepeatWrapping;
-metalTextureMap.wrapT = RepeatWrapping;
-metalTextureMap.matrixAutoUpdate = true;
+export const metalTextureMap = loadTexture(imgUrlmetalTextureMap)
+metalTextureMap.rotation = Math.PI / 2
+metalTextureMap.wrapS = RepeatWrapping
+metalTextureMap.wrapT = RepeatWrapping
+metalTextureMap.matrixAutoUpdate = true
 
-export const metalTextureRoughness = loadTexture("./src/assets/metal/metal-roughness.png");
-metalTextureMap.rotation = Math.PI / 2;
-metalTextureMap.wrapS = RepeatWrapping;
-metalTextureMap.wrapT = RepeatWrapping;
-metalTextureMap.matrixAutoUpdate = true;
+export const metalTextureRoughness = loadTexture(imgUrlmetalTextureRoughness)
+metalTextureMap.rotation = Math.PI / 2
+metalTextureMap.wrapS = RepeatWrapping
+metalTextureMap.wrapT = RepeatWrapping
+metalTextureMap.matrixAutoUpdate = true
 
-export const metalTextureHeight = loadTexture("./src/assets/metal/metal-height.png");
-metalTextureMap.rotation = Math.PI / 2;
-metalTextureMap.wrapS = RepeatWrapping;
-metalTextureMap.wrapT = RepeatWrapping;
-metalTextureMap.matrixAutoUpdate = true;
+export const metalTextureHeight = loadTexture(imgUrlmetalTextureHeight)
+metalTextureMap.rotation = Math.PI / 2
+metalTextureMap.wrapS = RepeatWrapping
+metalTextureMap.wrapT = RepeatWrapping
+metalTextureMap.matrixAutoUpdate = true
 
-export const metalTextureWide = loadTexture("./src/assets/metal/metal-wide.png");
-metalTextureWide.rotation = -Math.PI;
-metalTextureWide.wrapS = RepeatWrapping;
-metalTextureWide.wrapT = RepeatWrapping;
-metalTextureWide.matrixAutoUpdate = true;
+export const metalTextureWide = loadTexture(imgUrlmetalTextureWide)
+metalTextureWide.rotation = -Math.PI
+metalTextureWide.wrapS = RepeatWrapping
+metalTextureWide.wrapT = RepeatWrapping
+metalTextureWide.matrixAutoUpdate = true
 
-export const metalTextureWideMap = loadTexture("./src/assets/metal/metal-wide-normal.png");
-metalTextureWideMap.rotation = -Math.PI;
-metalTextureWideMap.wrapS = RepeatWrapping;
-metalTextureWideMap.wrapT = RepeatWrapping;
-metalTextureWideMap.matrixAutoUpdate = true;
+export const metalTextureWideMap = loadTexture(imgUrlmetalTextureWideMap)
+metalTextureWideMap.rotation = -Math.PI
+metalTextureWideMap.wrapS = RepeatWrapping
+metalTextureWideMap.wrapT = RepeatWrapping
+metalTextureWideMap.matrixAutoUpdate = true
 
-export const woodTexture = loadTexture("./src/assets/wood/wood.png");
-woodTexture.rotation = Math.PI / 2;
-woodTexture.wrapS = RepeatWrapping;
-woodTexture.wrapT = RepeatWrapping;
-woodTexture.matrixAutoUpdate = true;
+export const woodTexture = loadTexture(imgUrlwoodTexture)
+woodTexture.rotation = Math.PI / 2
+woodTexture.wrapS = RepeatWrapping
+woodTexture.wrapT = RepeatWrapping
+woodTexture.matrixAutoUpdate = true
 
-export const woodTextureMap = loadTexture("./src/assets/wood/wood-normal.png");
-woodTextureMap.rotation = Math.PI / 2;
-woodTextureMap.wrapS = RepeatWrapping;
-woodTextureMap.wrapT = RepeatWrapping;
-woodTextureMap.matrixAutoUpdate = true;
+export const woodTextureMap = loadTexture(imgUrlwoodTextureMap)
+woodTextureMap.rotation = Math.PI / 2
+woodTextureMap.wrapS = RepeatWrapping
+woodTextureMap.wrapT = RepeatWrapping
+woodTextureMap.matrixAutoUpdate = true
 
-export const woodTextureWide = loadTexture("./src/assets/wood/wood-wide.png");
-woodTexture.rotation = Math.PI / 2;
-woodTexture.wrapS = RepeatWrapping;
-woodTexture.wrapT = RepeatWrapping;
-woodTexture.matrixAutoUpdate = true;
+export const woodTextureWide = loadTexture(imgUrlwoodTextureWide)
+woodTexture.rotation = Math.PI / 2
+woodTexture.wrapS = RepeatWrapping
+woodTexture.wrapT = RepeatWrapping
+woodTexture.matrixAutoUpdate = true
 
-export const woodTextureWideMap = loadTexture("./src/assets/wood/wood-wide-normal.png");
-woodTextureMap.rotation = Math.PI / 2;
-woodTextureMap.wrapS = RepeatWrapping;
-woodTextureMap.wrapT = RepeatWrapping;
-woodTextureMap.matrixAutoUpdate = true;
+export const woodTextureWideMap = loadTexture(imgUrlwoodTextureWideMap)
+woodTextureMap.rotation = Math.PI / 2
+woodTextureMap.wrapS = RepeatWrapping
+woodTextureMap.wrapT = RepeatWrapping
+woodTextureMap.matrixAutoUpdate = true
 
-export const grassTexture = loadTexture("./src/assets/grass.jpg");
-grassTexture.wrapS = RepeatWrapping;
-grassTexture.wrapT = RepeatWrapping;
-grassTexture.repeat.set(5, 5);
-grassTexture.matrixAutoUpdate = true;
+export const grassTexture = loadTexture(imgUrlgrassTexture)
+grassTexture.wrapS = RepeatWrapping
+grassTexture.wrapT = RepeatWrapping
+grassTexture.repeat.set(5, 5)
+grassTexture.matrixAutoUpdate = true
 
-export const concreteTexture = loadTexture("./src/assets/concrete.jpg");
-concreteTexture.wrapS = RepeatWrapping;
-concreteTexture.wrapT = RepeatWrapping;
-concreteTexture.repeat.set(3, 3);
-concreteTexture.matrixAutoUpdate = true;
+export const concreteTexture = loadTexture(imgUrlconcreteTexture)
+concreteTexture.wrapS = RepeatWrapping
+concreteTexture.wrapT = RepeatWrapping
+concreteTexture.repeat.set(3, 3)
+concreteTexture.matrixAutoUpdate = true
 
 function loadTexture(url) {
   let texture = loader.load(
     url,
     function () {
-      console.log("texture loaded");
+      console.log('texture loaded')
     },
     function () {
-      console.log("loading textures");
+      console.log('loading textures')
     },
     function (err) {
-      console.log(err);
-    }
-  );
-  return texture;
+      console.log(err)
+    },
+  )
+  return texture
 }
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/configurator-custom/',
+  appType: 'custom',
   plugins: [vue()],
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg'],
 })
