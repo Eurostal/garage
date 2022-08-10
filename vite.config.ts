b6d8872cc8f616ebf4daf773d3d8b75c7144a968
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-08-10 16:02:39 +0200

    Test api added

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/configurator-custom/',
-  appType: 'custom',
-  plugins: [vue()],
+  plugins: [Vue()],
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg'],
   esbuild: {
-    minify: false,
     minifySyntax: false,
     minifyIdentifiers: false,
     minifyWhitespace: false,
   },
   build: {
     minify: false,
-    minifySyntax: false,
-    minifyIdentifiers: false,
-    minifyWhitespace: false,
   },
 })
