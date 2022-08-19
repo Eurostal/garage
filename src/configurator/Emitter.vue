d58a044b3f4afbcfad324e46ac9f2cd123c9ee61
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-08-19 22:37:59 +0200

    Change event listener

--- src/configurator/Emitter.vue
+++ src/configurator/Emitter.vue
@@ -10,10 +10,21 @@ export default {
         "Spad w lewo_4": "left",
         Dwuspadowy_0: "gable",
       },
+      gateNameTranslation: {
+        Dwuskrzydłowa_0: "double",
+        Uchylna_1: "tilted",
+        "Uchylna - szeroki panel_2": "wide",
+        "Bez bramy - otwarty przód_3": "empty",
+        "Zamiast bramy ściana_4": "",
+      },
+      gateStyleTranslation: {
+        Pionowe_0: "",
+        Poziome_1: "_H",
+      },
     };
   },
   methods: {
-    changeRoof: function ({ name = "Dwuspadowy_0", action = "update" }) {
+    changeRoof: function (name = "Dwuspadowy_0", { action = "update" }) {
       let object = {
         type: "roof",
         roofType: this.roofNameTranslation[name],
@@ -21,16 +32,16 @@ export default {
       this.$store.commit(action, { ...object });
     },
     changeRoofEvent: function (e) {
-      this.changeRoof({ name: e.target.value });
+      this.changeRoof(e.target.value);
     },
-    changeGarageSize: function ({ width = 0, length = 0 }) {
-      let object = { width: width, length: length };
-      if (!width) {
-        delete object.width;
+    changeGarageSize: function ({ width = false, length = false }) {
+      let object = {};
+      if (width) {
+        object.width = width;
       }
 
-      if (!length) {
-        delete object.length;
+      if (length) {
+        object.length = length;
       }
 
       if (width || length) {
@@ -57,6 +68,104 @@ export default {
           'div[data-uniqid="627a9ad625d4b6.52719206"] .tc-epo-label'
         ).textContent = e.target.value.split("_")[0];
     },
+    changeGate: function (
+      name,
+      {
+        type = false,
+        width = false,
+        height = false,
+        position = false,
+        style = "",
+      }
+    ) {
+      object = {};
+      object.type = "gate";
+      object.name = name;
+      object.wallId = 0;
+
+      if (type) {
+        object.gateType = this.gateNameTranslation[type];
+      }
+
+      if (width) {
+        object.width = width;
+      }
+
+      if (height) {
+        object.height = height;
+      }
+
+      if (position) {
+        object.x = position;
+      }
+
+      if (style) {
+        object.style = this.gateNameTranslation[type];
+      }
+
+      if (object.gateType) {
+        this.$store.commit("update", { ...object });
+      } else {
+        this.$store.commit("remove", { ...object });
+      }
+    },
+    changeGate1Event: function (e) {
+      this.changeGate("gate1");
+    },
+    changeGate2Event: function (e) {
+      this.changeGate("gate2");
+    },
+    changeGate1PositionEvent: function (e) {
+      this.changeGate("gate1", {
+        position: e.target.value / 100,
+      });
+    },
+    changeGate2PositionEvent: function (e) {
+      this.changeGate("gate2", {
+        position: e.target.value / 100,
+      });
+    },
+    changeGate1SizeEvent: function (e) {
+      this.changeGate("gate1", {
+        position: e.target.value / 100,
+      });
+    },
+    changeGate2SizeEvent: function (e) {
+      this.changeGate("gate2", {
+        position: e.target.value / 100,
+      });
+    },
+    changeGate1WidthEvent: function (e) {
+      this.changeGate("gate1", {
+        width: e.target.value / 100,
+      });
+    },
+    changeGate2WidthEvent: function (e) {
+      this.changeGate("gate2", {
+        width: e.target.value / 100,
+      });
+    },
+    changeGate1HeightEvent: function (e) {
+      this.changeGate("gate1", {
+        height: e.target.value / 100,
+      });
+    },
+    changeGate2HeightEvent: function (e) {
+      this.changeGate("gate2", {
+        height: e.target.value / 100,
+      });
+    },
+    changeGate1StyleEvent: function (e) {
+      this.changeGate("gate1", {
+        style: e.target.value,
+      });
+    },
+    changeGate2StyleEvent: function (e) {
+      this.changeGate("gate2", {
+        style: e.target.value,
+      });
+    },
+
     changeWindow: function ({ name, action = true }) {
       let object = {
         type: "window",
@@ -955,6 +1064,86 @@ export default {
         .querySelectorAll('div[data-uniqid="626932f0482669.99437527"] select')
         .forEach((input) =>
           input.addEventListener("change", this.changeGarageLengthEvent, {
+            passive: true,
+          })
+        );
+
+      form
+        .querySelectorAll('div[data-uniqid="625928cfacd5e1.56204472"] input')
+        .forEach((input) =>
+          input.addEventListener("change", this.changeGate1Event, {
+            passive: true,
+          })
+        );
+
+      form
+        .querySelectorAll('div[data-uniqid="627b7715c54f09.72204841"] input')
+        .forEach((input) =>
+          input.addEventListener("change", this.changeGate2Event, {
+            passive: true,
+          })
+        );
+
+      form
+        .querySelectorAll('div[data-uniqid="625928cfacd5f2.48728982"] input')
+        .forEach((input) =>
+          input.addEventListener("change", this.changeGate1WidthEvent, {
+            passive: true,
+          })
+        );
+
+      form
+        .querySelectorAll('div[data-uniqid="627b7720c54f10.62015977"] input')
+        .forEach((input) =>
+          input.addEventListener("change", this.changeGate2WidthEvent, {
+            passive: true,
+          })
+        );
+
+      form
+        .querySelectorAll('div[data-uniqid="625928cfacd608.04744343"] input')
+        .forEach((input) =>
+          input.addEventListener("change", this.changeGate1HeightEvent, {
+            passive: true,
+          })
+        );
+
+      form
+        .querySelectorAll('div[data-uniqid="627b772ac54f28.86727207"] input')
+        .forEach((input) =>
+          input.addEventListener("change", this.changeGate2HeightEvent, {
+            passive: true,
+          })
+        );
+
+      form
+        .querySelectorAll('div[data-uniqid="625929fa7219b1.06715193"] input')
+        .forEach((input) =>
+          input.addEventListener("change", this.changeGate1StyleEvent, {
+            passive: true,
+          })
+        );
+
+      form
+        .querySelectorAll('div[data-uniqid="627b7732c54f35.77583366"] input')
+        .forEach((input) =>
+          input.addEventListener("change", this.changeGate2StyleEvent, {
+            passive: true,
+          })
+        );
+
+      form
+        .querySelectorAll('div[data-uniqid="62ffad9da82021.94156944"] input')
+        .forEach((input) =>
+          input.addEventListener("change", this.changeGate1PositionEvent, {
+            passive: true,
+          })
+        );
+
+      form
+        .querySelectorAll('div[data-uniqid="62ffaf77735284.94935292"] input')
+        .forEach((input) =>
+          input.addEventListener("change", this.changeGate2PositionEvent, {
             passive: true,
           })
         );
