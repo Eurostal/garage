92a26f1514162c04c83486a9ed822f9453654f7c
Kamil <kamil@webo.agency>
2022-09-02 16:14:34 +0200

    materials basic test fix

--- src/configurator/Emitter.vue
+++ src/configurator/Emitter.vue
@@ -673,7 +673,7 @@ export default {
     changeSheetOrientation: function (e) {
       let object = {};
       if (document.querySelector('[data-uniqid="6268636725a2c5.41006929"] select').value === "Jednolity kolor_0") {
-        if (document.querySelector('[data-uniqid="6267c6616c1022.10988552"] input:checked').value !== undefined) {
+        if (document.querySelector('[data-uniqid="6267c6616c1022.10988552"] input:checked') !== undefined) {
           document.querySelectorAll('[data-uniqid="6267c6616c1022.10988552"] input:checked').forEach((input) => {
             object.material = input.value.split("_")[0].replace(" ", "");
           });
@@ -683,7 +683,7 @@ export default {
         object.type = "roof";
         this.$store.commit("updateMaterial", { ...object });
       } else {
-        if (document.querySelector('[data-uniqid="6267c6836c1098.51557180"] input:checked').value !== undefined) {
+        if (document.querySelector('[data-uniqid="6267c6836c1098.51557180"] input:checked') !== undefined) {
           document.querySelectorAll('[data-uniqid="6267c6836c1098.51557180"] input:checked').forEach((input) => {
             object.material = input.value.split("_")[0].replace(" ", "");
           });
