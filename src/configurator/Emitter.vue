1f051e73aac2f4402fa4abfc8ba64760fc8d09f5
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-09-02 12:09:33 +0200

    Change event listener

--- src/configurator/Emitter.vue
+++ src/configurator/Emitter.vue
@@ -339,9 +339,11 @@ export default {
     },
     changeFittingsMaterialEvent: function (e) {
       if(e.target.checked){
-        document.querySelector("form.cart").querySelectorAll('div[data-uniqid="6267a577abc002.73453783"] input').forEach((input) => {
-          input.checked = false;
-          input.dispatchEvent(new Event("change"));
+        document.querySelector("form.cart").querySelectorAll('div[data-uniqid="6267a577abc002.73453783"] input:checked').forEach((input) => {
+          if(input.id !== e.target.value){
+            input.checked = false;
+            input.dispatchEvent(new Event("change"));
+          }
         })
 
         var value = e.target.value.split("_")[0].replace(" ", "");
