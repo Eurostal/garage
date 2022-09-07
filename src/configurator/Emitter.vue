c75e8f407aa951112839a0e3789df24158b9a0c9
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-09-07 10:55:08 +0200

    Doors emit event handle

--- src/configurator/Emitter.vue
+++ src/configurator/Emitter.vue
@@ -648,7 +648,7 @@ export default {
       }
 
       if (handle) {
-        object.handle = this.handleNameTranslations[handle];;
+        object.handleSide = this.handleNameTranslations[handle];;
       }
 
       this.$store.commit(action ? "update" : "remove", { ...object });
@@ -780,6 +780,14 @@ export default {
           x: parseFloat(e) < 1 ? 0 : parseFloat(e) / 100,
         });
       }
+    },
+
+    changeDoor2HandleEvent: function (e) {
+      let wallId = this.selectWall(document.querySelector('div[data-uniqid="6267b951ff8d63.21986646"] select').value);
+      this.changeDoor("door2", {
+        wallId: wallId,
+        handle: e.target.value,
+      });
     },
 
     selectWall(input) {
@@ -1347,6 +1355,11 @@ export default {
       //door2
       form.querySelectorAll('div[data-uniqid="6267a72c905212.61206813"] input').forEach((input) =>
         input.addEventListener("change", this.changeDoor2Event, {
+          passive: true,
+        })
+      );
+      form.querySelectorAll('div[data-uniqid="6267a72c905227.37498764"] select').forEach((input) =>
+        input.addEventListener("change", this.changeDoor2HandleEvent, {
           passive: true,
         })
       );
