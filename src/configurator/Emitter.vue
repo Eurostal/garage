b9e9f777780f276704533ec064a898956106690d
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-09-14 15:59:36 +0200

    EUROSTALL-63 Doors default options

--- src/configurator/Emitter.vue
+++ src/configurator/Emitter.vue
@@ -729,15 +729,17 @@ export default {
         action: e.target.checked,
       });
 
-      document
-        .querySelector("form.cart")
-        .querySelectorAll('div[data-uniqid="62666bae882dd8.31559354"] select')
-        .forEach((input) => input.dispatchEvent(new Event("change")));
+      if(e.target.checked){
+        document
+          .querySelector("form.cart")
+          .querySelectorAll('div[data-uniqid="62666bae882dd8.31559354"] select')
+          .forEach((input) => input.dispatchEvent(new Event("change")));
 
-      document
-        .querySelector("form.cart")
-        .querySelectorAll('div[data-uniqid="6267b940ff8d56.42586482"] select')
-        .forEach((input) => input.dispatchEvent(new Event("change")));
+        document
+          .querySelector("form.cart")
+          .querySelectorAll('div[data-uniqid="6267b940ff8d56.42586482"] select')
+          .forEach((input) => input.dispatchEvent(new Event("change")));
+      }
     },
 
     changeDoor1WallEvent: function (e) {
@@ -820,15 +822,17 @@ export default {
         action: e.target.checked,
       });
 
-      document
-        .querySelector("form.cart")
-        .querySelectorAll('div[data-uniqid="6267a72c905227.37498764"] select')
-        .forEach((input) => input.dispatchEvent(new Event("change")));
+      if(e.target.checked){
+        document
+          .querySelector("form.cart")
+          .querySelectorAll('div[data-uniqid="6267a72c905227.37498764"] select')
+          .forEach((input) => input.dispatchEvent(new Event("change")));
 
-      document
-        .querySelector("form.cart")
-        .querySelectorAll('div[data-uniqid="62666bae882dd8.31559354"] select')
-        .forEach((input) => input.dispatchEvent(new Event("change")));
+        document
+          .querySelector("form.cart")
+          .querySelectorAll('div[data-uniqid="62666bae882dd8.31559354"] select')
+          .forEach((input) => input.dispatchEvent(new Event("change")));
+      }
     },
 
     changeDoor2WallEvent: function (e) {
