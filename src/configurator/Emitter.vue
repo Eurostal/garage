3e7586cb5714a355d45311556942c7ac624489ce
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-09-16 15:34:12 +0200

    EUROSTALL-62 Default values

--- src/configurator/Emitter.vue
+++ src/configurator/Emitter.vue
@@ -173,6 +173,7 @@ export default {
 
       //Disable gate2 if gate1 is
       if (!this.gateNameTranslation[e.target.value] != "") {
+        document.querySelector('div[data-uniqid="627b7715c54f09.72204841"] input[value="Zamiast bramy ściana_4"]').click();
         this.changeGate("gate2", object);
       }
 
