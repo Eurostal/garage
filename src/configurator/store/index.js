66b17e8094cf3799df1d3c46a0a704248fe5294e
Kamil <kamil@webo.agency>
2022-10-04 14:27:58 +0200

    last commit fix test

--- src/configurator/store/index.js
+++ src/configurator/store/index.js
@@ -411,9 +411,9 @@ function fillData(data) {
   };
 
   if (data.type === "gate") {
-    // if (data.gateType == "tilted" || data.gateType == "wide") {
-    //   filledData.width = filledData.width - 0.2;
-    // }
+    if (data.gateType == "tilted" || data.gateType == "wide") {
+      filledData.width = filledData.width - 0.2;
+    }
     if (data.width && (filledData.gateType == "tilted" || filledData.gateType == "wide")) {
       data.width = data.width - 0.2;
     }
