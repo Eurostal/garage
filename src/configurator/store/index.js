7eacda73cd1105c76389441e6a636ef620377ad8
Kamil <kamil@webo.agency>
2022-10-11 13:00:19 +0200

    contains function gate fix test y check removed

--- src/configurator/store/index.js
+++ src/configurator/store/index.js
@@ -390,7 +390,7 @@ function contains(element, { x, y }) {
     height: element.height,
   };
   if (element.type == "gate") {
-    return roundTwoDec(rect.x) < x && x < roundTwoDec(rect.x + rect.width);
+    return roundTwoDec(rect.x) <= x && x < roundTwoDec(rect.x + rect.width);
   } else {
     return (
       roundTwoDec(rect.x - 0.2) < x &&
