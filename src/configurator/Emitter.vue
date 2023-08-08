7cff86c7297a4303163f2c6db608c6e2d68d8cc6
Kamil <kamil@webo.agency>
2023-08-08 13:56:34 +0200

    hiddenKeys update

--- src/configurator/Emitter.vue
+++ src/configurator/Emitter.vue
@@ -1424,7 +1424,7 @@ console.log('----' + material + '-------');
       const textareaRaw = document.querySelector('input[name="raw-garage-config"]');
       const actualGarage = this.$store.getters.getGarage;
       const formData = new FormData(document.querySelector("form.cart"));
-      const hiddenKeys = ["tcaddtocart", "tm-epo-counter", "quantity", "cpf_product_price", "tc_form_prefix"];
+      const hiddenKeys = ["tcaddtocart", "tm-epo-counter", "quantity", "cpf_product_price", "tc_form_prefix", "yith_wapo_is_single","gtm4wp_id","gtm4wp_sku","gtm4wp_name","gtm4wp_price","gtm4wp_category","gtm4wp_stocklevel"];
       let formDataText = "";
 
       for (const pair of formData.entries()) {
