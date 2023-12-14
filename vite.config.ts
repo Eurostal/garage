4a66f9e0867127d11487800a80bf7d84bcc64d0c
Kamil <kamil@webo.agency>
2023-12-14 10:39:13 +0100

    vite update, hash in separate file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-const path = require('path')
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+const generateHash = () => {
+  const hash = crypto.createHash("shake256", { outputLength: 4 }).update(Date.now().toString()).digest('hex');
+  return hash;
+};
+
+const generateHashFilePlugin = () => ({
+  name: 'generate-hash-file',
+  writeBundle(){
+    const hash = generateHash()
+    console.log(hash);
+    fs.writeFileSync('dist/assets/hash.env', `HASH=${hash}`);
+  },
+
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/configurator-custom/',
-  plugins: [Vue()],
+  plugins: [Vue(),generateHashFilePlugin()],
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.webp'],
   esbuild: {
     minifySyntax: false,
@@ -14,5 +31,10 @@ export default defineConfig({
   },
   build: {
     minify: false,
+    rollupOptions:{
+      output:{
+        entryFileNames: `assets/configurator-engine.js`
+      }
+    }
   },
 })
