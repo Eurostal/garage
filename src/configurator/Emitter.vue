6390d47faa62683462214d7be8b22568046b6ae4
Kamil <kamil@webo.agency>
2023-12-14 14:52:06 +0100

    translate all alerts

--- src/configurator/Emitter.vue
+++ src/configurator/Emitter.vue
@@ -1477,7 +1477,6 @@ console.log('----' + material + '-------');
   },
 
   mounted: function () {
-    this.$translate('tłumaczenie w komponencie!')
     var form = document.querySelector("form.cart");
     if (document.querySelector("form.cart")) {
       // form.addEventListener("change", this.changeEventForm, { passive: true });
